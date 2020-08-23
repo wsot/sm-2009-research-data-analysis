02d4e944ce57fe8c06ed8ba763d49949791744dd
Simeon J Morgan <smorgan@digitalfeed.net>
2020-08-23 17:28:18 +1000

    Setting up structure

---
+++ tasks.py
@@ -0,0 +1,37 @@
+import pathlib
+import sys
+
+import invoke
+
+
+@invoke.task
+def format(ctx):
+    """
+    Apply automatic code formating tools
+    """
+    ctx.run("black src")
+    ctx.run("isort ")
+
+
+@invoke.task
+def typing(ctx):
+    """
+    Check type annotations
+    """
+    ctx.run("mypy src")
+
+
+@invoke.task
+def lint(ctx):
+    """
+    Check linting in the src folder
+    """
+    ctx.run("flake8 src")
+
+
+@invoke.task(format, typing, lint)
+def check(ctx):
+    """
+    Runs all the code checking tools
+    """
+    ...
